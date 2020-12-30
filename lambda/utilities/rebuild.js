e3c76c68c6c3fc06351b072374220c5770f105ae
Bryan Robinson <bryanrobinson@gmail.com>
2020-12-30 11:56:37 -0500

    Updates rebuild hook

--- lambda/utilities/rebuild.js
+++ lambda/utilities/rebuild.js
@@ -3,7 +3,7 @@ const rp = require('request-promise');
 const rebuildSite = async function() {
     var options = {
         method: 'POST',
-        uri: 'https://api.netlify.com/build_hooks/5d7fa6175504dfd43377688c',
+        uri: 'https://api.netlify.com/build_hooks/5fecb1312e58a3009b3f937b',
         body: {},
         json: true      
       };
